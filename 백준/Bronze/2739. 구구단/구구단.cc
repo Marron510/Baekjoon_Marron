67a23a5a3aa5b630243f1ea21c7fab4c66f6ccb6
Marron510 <wkdtl345@naver.com>
2024-09-30 18:48:17 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a = 0;
+
+	std::cin >> a;
+
+	std::cout << a << " * " << "1" << " = " << a * 1 << std::endl;
+	std::cout << a << " * " << "2" << " = " << a * 2 << std::endl;
+	std::cout << a << " * " << "3" << " = " << a * 3 << std::endl;
+	std::cout << a << " * " << "4" << " = " << a * 4 << std::endl;
+	std::cout << a << " * " << "5" << " = " << a * 5 << std::endl;
+	std::cout << a << " * " << "6" << " = " << a * 6 << std::endl;
+	std::cout << a << " * " << "7" << " = " << a * 7 << std::endl;
+	std::cout << a << " * " << "8" << " = " << a * 8 << std::endl;
+	std::cout << a << " * " << "9" << " = " << a * 9 << std::endl;
+}
