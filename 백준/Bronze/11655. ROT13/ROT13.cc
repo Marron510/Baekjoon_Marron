6503756c3c9684e452eb40fff55c7861e32fcac2
Marron510 <wkdtl345@naver.com>
2025-05-14 18:39:54 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+int main()
+{
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] >= 65 && str[i] < 92)
+		{
+			if ((str[i] + 13) > 90)
+			{
+				str[i]  = str[i] + 13 - 26;
+			}
+			else
+			{
+				str[i] = str[i] + 13;
+			}
+		}
+		else if (str[i] >= 97 && str[i] <= 122)
+		{
+			if ((str[i] + 13) > 122)
+			{
+				str[i] = str[i] + 13 - 26;
+			}
+			else
+			{
+				str[i] = str[i] + 13;
+			}
+		}
+	}
+
+	cout << str;
+}
