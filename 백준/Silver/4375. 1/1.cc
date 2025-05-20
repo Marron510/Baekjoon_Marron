ab59186d0fbccd85f77376a18cbaad938f4ee4bb
Marron510 <wkdtl345@naver.com>
2025-05-20 10:49:13 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll n ;
+
+int main()
+{
+	while (cin >> n)
+	{
+		int ret = 1, cnt = 1;
+		while (true)
+		{
+			if ((ret % n) == 0)
+			{
+				cout << cnt << "\n";
+				break;
+			}
+			else
+			{
+				ret = (ret * 10) + 1;
+				ret %= n;
+				cnt++;
+			}
+		}
+	}
+}
