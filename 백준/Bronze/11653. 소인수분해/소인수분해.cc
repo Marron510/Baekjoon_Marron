4726640a97d108caf0e466efa6068d7211f4f6fa
Marron510 <wkdtl345@naver.com>
2025-06-21 04:53:11 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int N;
+	cin >> N;
+
+	int i = 2;
+	
+	while(N != 1)
+	{
+		if (N % i == 0) 
+        {
+			cout << i << "\n";
+			N /= i;
+		}
+		else 
+        {
+           i++;
+        }
+	}
+    
+    return 0;
+}
