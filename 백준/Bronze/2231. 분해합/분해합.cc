981ff388578d25dfeef697cfcd7eb125c4d35c6d
Marron510 <wkdtl345@naver.com>
2025-07-09 04:21:49 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    ios::sync_with_stdio(NULL), cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    for (int i = 1; i < N; i++) 
+    {
+        int a = i;
+        int sum = i;
+
+        while (a != 0) 
+        {
+            sum = sum + a % 10;
+            a = a / 10;
+        }
+
+        if (sum == N) 
+        {
+            cout << i << "\n";
+            return 0;
+        }
+    }
+    cout << 0 << '\n';
+    
+    return 0;
+}
