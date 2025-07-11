2ddf354568a3bae2b5678113cf5a861d358bde4d
Marron510 <wkdtl345@naver.com>
2025-07-11 13:19:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+    
+	int cnt = 0;
+	int num = 665;		
+	
+	while (true)
+	{
+		num++;
+		int comp = num;
+        
+		while (comp >= 666)
+		{
+			if (comp % 1000 == 666)
+			{
+				cnt++;
+				break;
+			}
+            
+			comp /= 10;
+		}
+        
+		if (cnt == n) 
+		{
+			cout << num << "\n";
+			break;
+		}
+	}
+
+	return 0;
+}
