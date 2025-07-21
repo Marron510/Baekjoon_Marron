34d1ba770932cfb3ee37fef9d307daf610872bb8
Marron510 <wkdtl345@naver.com>
2025-07-22 05:57:47 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+	queue<int>q;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string command;
+		cin >> command;
+
+		if (command == "push")
+		{
+			int num;
+			cin >> num;
+			q.push(num);
+		}
+		else if (command == "pop")
+		{
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (command == "size")
+		{
+			cout << q.size() << '\n';
+		}
+		else if (command == "empty")
+		{
+			cout << q.empty() << '\n';
+		}
+		else if (command == "front")
+		{
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+				cout << q.front() << '\n';
+		}
+		else if (command == "back")
+		{
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+				cout << q.back() << '\n';
+		}
+	}
+	return 0;
+}
