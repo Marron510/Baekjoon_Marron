2a987551ab467a1d8da46b8c53960e49424e9a98
Marron510 <wkdtl345@naver.com>
2025-07-29 06:41:11 +0900

    [Silver IV] Title: 수 찾기, Time: 144 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> list;
+vector<int> vec;
+
+int go(int value, int v1, int v2)
+{
+	if (v1 > v2)
+    {
+        return 0;
+    }
+
+	int mid = (v1 + v2) / 2;
+
+	if (value == list.at(mid))
+    {
+        return 1;
+    }
+	else if (value > list.at(mid))
+    {
+        return go(value, mid + 1, v2);
+    }
+	else
+    {
+        return go(value, v1, mid - 1);
+    }
+}
+
+int main()
+{
+	int N, M;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		list.push_back(temp);
+	}
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		vec.push_back(temp);
+	}
+
+	sort(list.begin(), list.end());
+
+	for (int i = 0; i < M; i++)
+		cout << go(vec[i], 0, N - 1) << '\n';
+
+	return 0;
+}
